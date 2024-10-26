1c638787b2c46d3b59d503efe80b360eb83df167
Saurav Upadhyay <sauravup041103@gmail.com>
2024-10-27 02:10:21 +0530

    add: home route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider , createBrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  }
+])
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
